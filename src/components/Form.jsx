1fd7d8ec33e6a3ac55314d2fa6116865d854a007
psdekate <piyushdekate29@gmail.com>
2025-03-10 20:11:48 +0530

    form validations

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,15 +6,6 @@ export default function Form() {
   const enteredPassword = useRef();
 
   const [emailIsInvalid, setEmailIsInvalid] = useState(false);
-  //   const [enteredValues, setEnteredValues] = useState({
-  //     email: "",
-  //     password: "",
-  //   });
-
-  //   const [didEdit, setDidEdit] = useState({
-  //     email: false,
-  //     password: false,
-  //   });
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -31,48 +22,16 @@ export default function Form() {
     console.log("Sending HTTP");
   }
 
-  //   function handleSubmittedValues(identifier, value) {
-  //     setEnteredValues((prevValue) => ({
-  //       ...prevValue,
-  //       [identifier]: value,
-  //     }));
-  //     setDidEdit((prevValue) => ({
-  //       ...prevValue,
-  //       [identifier]: false,
-  //     }));
-  //   }
-
-  //   function handleEditState(identifier) {
-  //     setDidEdit((prevValue) => ({
-  //       ...prevValue,
-  //       [identifier]: true,
-  //     }));
-  //     console.log(didEdit);
-  //   }
-
   return (
     <form onSubmit={handleSubmit} className="form-wrapper">
       <div className="form-element">
         <label htmlFor="email">email</label>
-        <input
-          type="text"
-          name="email"
-          //   value={enteredValues.email}
-          //   onBlur={() => handleEditState("email")}
-          //   onChange={(event) => handleSubmittedValues("email", event.target.value)}
-          ref={enteredEmail}
-        />
+        <input type="text" name="email" ref={enteredEmail} />
         {emailIsInvalid && <p>Enter a valid email</p>}
       </div>
       <div className="form-element">
         <label htmlFor="password">password</label>
-        <input
-          type="password"
-          name="password"
-          //   value={enteredValues.password}
-          //   onChange={(event) => handleSubmittedValues("password", event.target.value)}
-          ref={enteredPassword}
-        />
+        <input type="password" name="password" ref={enteredPassword} />
       </div>
       <div className="buttons">
         <button className="submit">Submit</button>
